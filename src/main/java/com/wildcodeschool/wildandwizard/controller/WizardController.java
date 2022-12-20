6bac18f7a168ba288f08814907791b660528dde6
blandine <blandine.mallart@gmail.com>
2022-12-20 15:52:53 +0100

    fusion insert in select

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -4,7 +4,10 @@ import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Date;
 
 @Controller
 public class WizardController {
@@ -33,5 +36,20 @@ public class WizardController {
         model.addAttribute("wizards", repository.findByLastName(lastName));
 
         return "wizard_get_all";
+    }
+
+    @PostMapping("/wizard/create")
+    public String postWizard(Model model,
+                             @RequestParam String firstName,
+                             @RequestParam String lastName,
+                             @RequestParam Date birthday,
+                             @RequestParam String birthPlace,
+                             @RequestParam(required = false, defaultValue = "") String biography,
+                             @RequestParam(required = false, defaultValue = "false") boolean muggle
+    ) {
+        model.addAttribute("wizard", repository.save(firstName, lastName,
+                birthday, birthPlace, biography, muggle));
+
+        return "wizard_get";
     }
 }
